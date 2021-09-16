71d79a5cfae609b1a694e839cab1b6f8eab4740e
Sebastian Cheng <sebast1908@gmail.com>
2021-09-16 19:25:28 +0200

    Updated models for API

--- task2/src/models/bicycle.ts
+++ task2/src/models/bicycle.ts
@@ -1,5 +1,30 @@
 export interface Station {
-  station_id: number;
+  station_id: string;
+  name: string;
+  address: string;
+  lat: number;
+  lon: number;
+  capacity: number;
+}
+
+export interface Available {
+  is_installed: number;
+  is_renting: number;
+  num_bikes_available: number;
+  num_docks_available: number;
+  last_reported: number;
+  is_returning: number;
+  station_id: string;
+}
+
+export interface StationStatus {
+  station_id: string;
+  is_installed: number;
+  is_renting: number;
+  num_bikes_available: number;
+  num_docks_available: number;
+  last_reported: number;
+  is_returning: number;
   name: string;
   address: string;
   lat: number;
