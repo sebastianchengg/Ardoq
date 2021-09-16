a3950f7f840435ba06713fb9c7877d6d004a53db
Sebastian Cheng <sebast1908@gmail.com>
2021-09-16 19:25:15 +0200

    Created the map with markers

--- task2/src/components/MapComponent.tsx
+++ task2/src/components/MapComponent.tsx
@@ -1,19 +1,59 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
 import BicycleAPI from "../api/bicycle";
-import { Station } from "../models/bicycle";
+import { StationStatus } from "../models/bicycle";
+import { GoogleMap, useJsApiLoader, InfoWindow } from "@react-google-maps/api";
+import "./MapComponent.css";
+
+const MAPS_API_KEY = "AIzaSyDDb_X4JA1smd-RCg-UhY-fSidxs6RuF4g";
+
+const containerStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+
+const osloCenter = {
+  lat: 59.92,
+  lng: 10.75,
+};
 
 export const MapComponent: FunctionComponent = () => {
-  const [stations, setstations] = useState<Station[]>([]);
+  const [stations, setstations] = useState<StationStatus[]>([]);
 
   useEffect(() => {
     BicycleAPI.getStations().then((stations) => setstations(stations));
   }, []);
+  
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: MAPS_API_KEY,
+  });
 
-  console.log(stations[0])
+  const renderMarkers = () => {
+    return stations.map((station) => (
+        <InfoWindow
+          position={{ lat: station.lat, lng: station.lon }}
+          key={`infowindow-${station.station_id}`}
+        >
+          <div
+            key={`markertext-${station.station_id}`}
+            className={"bicycle-marker"}
+          >{`${station.num_bikes_available}/${
+            station.num_bikes_available + station.num_docks_available
+          }`}</div>
+        </InfoWindow>
+    ));
+  };
 
-  return (
-    <>
-      <h1>Map</h1>
-    </>
+  return isLoaded ? (
+    <GoogleMap
+      key={"map"}
+      mapContainerStyle={containerStyle}
+      center={osloCenter}
+      zoom={14.6}
+    >
+      {renderMarkers()}
+    </GoogleMap>
+  ) : (
+    <></>
   );
 };
--- task2/src/pages/MapPage.tsx
+++ task2/src/pages/MapPage.tsx
@@ -1,9 +1,12 @@
 import React, { FunctionComponent } from "react";
 import { MapComponent } from "../components/MapComponent";
+import logo from "../static/bysykkelLogo.png"
+import "./MapPage.css"
 
 const MapPage: FunctionComponent = () => {
   return (
     <>
+    <img src={logo} alt="" className="logo"/>
       <MapComponent />
     </>
   );
